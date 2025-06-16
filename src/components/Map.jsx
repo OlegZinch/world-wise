50af8cccd5422d582a1053457a27fd10cfd70c63
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-06-17 00:03:43 +0100

    added interacting with the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,30 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from 'react-leaflet'
 
 import useCities from '../hooks/useCities'
 
 import styles from './Map.module.css'
 
 function Map() {
-  const navigate = useNavigate()
-
   const { cities } = useCities()
 
   const [mapPosition, setMapPosition] = useState([40, 0])
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams] = useSearchParams()
 
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+  const mapLat = searchParams.get('lat')
+  const mapLng = searchParams.get('lng')
 
-  // onClick={() => navigate('form')}
+  useEffect(() => {
+    if (mapLat && mapLng) {
+      setMapPosition([mapLat, mapLng])
+    }
+  }, [mapLat, mapLng])
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
@@ -42,9 +51,26 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   )
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap()
+  map.setView(position)
+  return null
+}
+
+function DetectClick() {
+  const navigate = useNavigate()
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  })
+  return null
 }
 
 export default Map
